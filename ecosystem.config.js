a88984bd3b6d1b7f84c9bcb4d95ac6d4f0b6741e
Kingsintersect <kingsintersect@gmail.com>
2026-05-11 19:02:29 +0100

    Bug-Fix: remove typescript to devdependecies and add it to depenedecies - updated the ecocytem.config.js for only create one instance of the application instead of in clusters

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,9 @@ module.exports = {
         name: process.env.APP_NAME || 'learn.imt.qverselearning.org',
         script: 'node_modules/.bin/next',
         args: 'start',
-        instances: 1,
-        exec_mode: 'fork',
+        instances: 1, // or : instances: 'max',
+        exec_mode: 'fork', //or : exec_mode: 'cluster',        
+            
         // Next.js reads .env.production automatically when NODE_ENV=production.
         // PM2 only needs to set PORT and NODE_ENV here.
         env: {
